dc5e09a8d4a2b5eab5bd56bb1818cc95fb4e3137
oguzhansali61 <154914330+oguzhansali61@users.noreply.github.com>
2024-05-20 21:15:18 +0300

    .

--- src/TryCatch/Main.java
+++ src/TryCatch/Main.java
@@ -4,27 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static int[]arr={0,1,2,3,4,5,7,8,9,10};//Dizi manuel olarak tanımlandı.
+    private static int[] arr = {0, 1, 2, 3, 4, 5, 7, 8, 9, 10};//Dizi manuel olarak tanımlandı.
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Bir sayı seçiniz: ");
-        int indis= input.nextInt();
+        int indis = input.nextInt();
         System.out.println(checkArray(indis));//Hatanın ayıklandığı metot çağrıldı.
     }
-    public static String checkArray(int indis){//Alınan değer için hata çıktısı oluşturuldu.
 
-        try{
-            int number = arr[indis];
-            return "Listenin "+ indis + ". elamanı " +arr[indis]+"dir.";
-        }catch (ArrayIndexOutOfBoundsException e){
+    public static String checkArray(int indis) {//Alınan değer için hata çıktısı oluşturuldu.
+        try {
+            int number = arr[indis];//Kullanıcıdan alınan değer listenin içerisindemi bakılır.
+            return "Listenin " + indis + ". elamanı " + arr[indis] + "dir.";//İçindeyse çıktı oluşturulur.
+        } catch (ArrayIndexOutOfBoundsException e) {//Liste dışında ise hata yakalanır.
             System.out.println("--------------------");
             System.out.println("Hata yakalandı.");
             System.out.println(e.getMessage());
             System.out.println();
             return "0 ile 9 arasında bir değer giriniz.";
-
         }
-        //System.out.println("Program sonlandırıldı.");
+
     }
 }
