db784d7fbecb77e41644568fb1a980b019b01876
oguzhansali61 <154914330+oguzhansali61@users.noreply.github.com>
2024-05-20 21:12:27 +0300

    .

--- src/TryCatch/Main.java
+++ src/TryCatch/Main.java
@@ -3,19 +3,28 @@ package TryCatch;
 import java.util.Scanner;
 
 public class Main {
+
+    private static int[]arr={0,1,2,3,4,5,7,8,9,10};//Dizi manuel olarak tanımlandı.
+
     public static void main(String[] args) {
-        int[]arr={0,1,2,3,4,5,7,8,9,10};
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir sayı seçiniz: ");
+        int indis= input.nextInt();
+        System.out.println(checkArray(indis));//Hatanın ayıklandığı metot çağrıldı.
+    }
+    public static String checkArray(int indis){//Alınan değer için hata çıktısı oluşturuldu.
+
         try{
-            Scanner input = new Scanner(System.in);
-            System.out.println("Bir sayı seçiniz: ");
-            int indis= input.nextInt();
-            System.out.println("Listenin "+ indis + ". elamanı " +arr[indis]+"dir.");
+            int number = arr[indis];
+            return "Listenin "+ indis + ". elamanı " +arr[indis]+"dir.";
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("--------------------");
             System.out.println("Hata yakalandı.");
             System.out.println(e.getMessage());
             System.out.println();
+            return "0 ile 9 arasında bir değer giriniz.";
+
         }
-        System.out.println("Program sonlandırıldı.");
+        //System.out.println("Program sonlandırıldı.");
     }
 }
